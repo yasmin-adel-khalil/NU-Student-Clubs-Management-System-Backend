be913a86bb3802bd2951fac43fdb2039d5ad8162
Gehad Almolla <gehad.almolla@gmail.com>
2025-12-17 23:32:08 +0200

    feat(validation): add validation annotations to MembershipRequest (#15)

--- src/main/java/com/nu/clubs/clubs_bakend/dto/MembershipRequest.java
+++ src/main/java/com/nu/clubs/clubs_bakend/dto/MembershipRequest.java
@@ -1,5 +1,6 @@
 package com.nu.clubs.clubs_bakend.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MembershipRequest {
+    
+    @NotNull(message = "User ID is required")
     private Long userId;
+    
+    @NotNull(message = "Club ID is required")
     private Long clubId;
 }
