b0bdf6be5f767be7310c959663e9d4b23775f67d
Gehad Almolla <gehad.almolla@gmail.com>
2025-12-17 23:26:14 +0200

    feat(dto): add ClubResponse and MembershipRequest DTOs (#15.1), closes #30

--- src/main/java/com/nu/clubs/clubs_bakend/dto/ClubResponse.java
+++ src/main/java/com/nu/clubs/clubs_bakend/dto/ClubResponse.java
@@ -1,4 +1,21 @@
 package com.nu.clubs.clubs_bakend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClubResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String president;
+    private String email;
+    private String category;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/nu/clubs/clubs_bakend/dto/MembershipRequest.java
+++ src/main/java/com/nu/clubs/clubs_bakend/dto/MembershipRequest.java
@@ -1,4 +1,13 @@
 package com.nu.clubs.clubs_bakend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MembershipRequest {
+    private Long userId;
+    private Long clubId;
 }
