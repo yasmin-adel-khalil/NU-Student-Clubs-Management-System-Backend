fe34f4d4ed10bf5a3ade74ffb68f64085025c4c4
Gehad Almolla <gehad.almolla@gmail.com>
2025-12-19 00:21:33 +0200

    chore: remove unused import from ClubController

--- src/main/java/com/nu/clubs/clubs_bakend/controller/ClubController.java
+++ src/main/java/com/nu/clubs/clubs_bakend/controller/ClubController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
